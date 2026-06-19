dcfdbef7b8f9e27394594e3f0d14aab0614fc474
thierryrobert-beep <thierry.robert@beorntech.com>
2026-06-19 08:30:24 +0200

    Create helo.py

---
+++ helo.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+print("Bonjour Thierry !")
+print("Bienvenue sur ton premier projet GitHub.")
+
+aujourdhui = datetime.now()
+
+print("Nous sommes le", aujourdhui.strftime("%d/%m/%Y"))
